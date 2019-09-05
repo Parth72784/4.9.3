0a727c0f4d60123106a16583663c00bf89ce7b1e
Thakkar Parth <thakkarparth7598@outlook.com>
2019-09-05 10:44:41 +0530

    Add project files.

---
+++ 4-9.1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_4_9._1.Global" Language="C#" %>
---
+++ 4-9.1/WebForm1.aspx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _4_9._1
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        
+        protected void btn1_Click(object sender, EventArgs e)
+        {
+            if (!Directory.Exists(Server.MapPath("Images")))
+            {
+                Directory.CreateDirectory(Server.MapPath("Images"));
+            }
+
+
+            string imageUrl = (Server.MapPath("Images") + "\\" + img.FileName);
+            string dbpathImage = "~/Images/" + img.FileName;
+            img.SaveAs(imageUrl);
+        }
+    }
+}
